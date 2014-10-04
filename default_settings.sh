6b698498f2af6b8206d36ba8fda462144de2c8f7
brennj55 <john.brennan55@mail.dcu.ie>
2014-10-04 23:48:02 +0100

    Cleaning up just to show my work.

--- default_settings.sh
+++
@@ -1,9 +0,0 @@
-# This file contains a template for setting environment variables needed by certain scripts
-
-# Copy this file to settings.sh
-# Fill in appropriate values
-# Then run source settings.sh before running scripts or starting IPython
-
-export MONGO_USER=
-export MONGO_PWD=
-export MONGO_IP=
